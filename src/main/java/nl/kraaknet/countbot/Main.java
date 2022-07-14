b975141fb0689da2c9d94df190575842b876e2d5
Floris Kraak <randakar@gmail.com>
2022-07-14 22:26:13 +0200

    Fix Main.java
    Can't put it any other way. Maven needs things to work, raw javac calls aren't going to cut it and the main class should be in a very specific place - *not* the project root.

--- src/main/java/nl/kraaknet/countbot/Main.java
+++ src/main/java/nl/kraaknet/countbot/Main.java
@@ -1,9 +1,15 @@
+package nl.kraaknet.countbot;
+
+import discord4j.core.DiscordClient;
+import discord4j.core.event.domain.message.MessageCreateEvent;
+import discord4j.core.object.entity.Message;
+
 class Main {
     public static void main(String[] args) {
         setupPong(args[0]);
     }
 
-    private void setupPong(final String token) {
+    private static void setupPong(final String token) {
         final DiscordClient client = DiscordClient.create(token);
 
         client.login()
